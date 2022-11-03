1cb1934cebd370ecccc7b8f8a29e0d3f98c82512
Desmond Braxton Jr <braxtond232@gmail.com>
2022-11-03 09:02:11 -0400

    Android Development

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+var greeting: String = "Hello"
+var name: String = "Nate"
+
+fun main() {
+    println(greeting)
+    println(name)
+
+    greeting = "hi"
+    println(greeting)
+    println("The message has changed")
+}
+
+
+
